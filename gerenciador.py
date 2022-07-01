c3da3b3b6d26031fecfc68ed268c2546c9bb655e
tawhamjavascript <taw.ham.balbino9914@gmail.com>
2022-07-01 16:22:43 -0300

    version sem bug e comentado

--- gerenciador.py
+++ gerenciador.py
@@ -6,50 +6,49 @@ from Arquivo import Arquivo
 
 class Gerenciador:
     def __init__(self):
-        self.__arquivos = self.__montador_arquivos()
-        self.__caminho = "./arquivos/"
+        self.__arquivos = self.__montador_arquivos()  # guarda um dict com todos objetos Arquivos
+        self.__caminho = "./arquivos/"  # variável que vai guardar o caminho dos arquivos
 
     def __montador_arquivos(self):
-        arquivosLs = os.listdir("./arquivos")
-        arquivos = {}
-        for arquivo in arquivosLs:
-            arquivos.update({arquivo: Arquivo(arquivo)})
+        arquivosLs = os.listdir("./arquivos")  # guarda todos os arquivos presente no diretório arquivos
+        arquivos = {}  
+        for arquivo in arquivosLs:  
+            arquivos.update({arquivo: Arquivo(arquivo)}) # adiciona a cada interação o objeto arquivo no dicionário
 
-        return arquivos
+        return arquivos  # retorna o dict de objetos
 
     def criar_arquivo(self, arquivo):
-        caminho = self.__caminho + arquivo
-        arquivoExiste = os.path.isfile(caminho)
+        caminho = self.__caminho + arquivo  # guarda o caminho onde vai ser criado o arquivo
+        arquivoExiste = os.path.isfile(caminho)  # verifica se o arquivo existe
         try:
-            assert not arquivoExiste
-            arquivo_caminho = Path(caminho)
-            arquivo_caminho.touch()
-            self.__arquivos.update({arquivo: Arquivo(arquivo)})
+            assert not arquivoExiste  # se o arquivo existir emite um erro
+            arquivo_caminho = Path(caminho)  # Cria um objeto do tipo path 
+            arquivo_caminho.touch()  # cria o arquivo 
+            self.__arquivos.update({arquivo: Arquivo(arquivo)})  # adiciona o arquivo no dict
 
         except AssertionError:
-            raise ArquivoError("arquivo inexistente")
+            raise ArquivoError("arquivo inexistente")  # propaga um arquivoError
 
     def ler_arquivo(self, arquivo):
-        arquivo_objeto = self.__arquivos.get(arquivo)
+        arquivo_objeto = self.__arquivos.get(arquivo)  # pegar no dict o arquivo 
         try:
-            assert arquivo_objeto is not None
-            texto = arquivo_objeto.ler_arquivo()
-            return texto
+            assert arquivo_objeto is not None  # se o arquivo não existir solta um AssertionError
+            texto = arquivo_objeto.ler_arquivo()  # ler o conteúdo do arquivo
+            return texto  # retorna o conteúdo do arquivo
 
         except AssertionError:
 
-            raise ArquivoError("arquivo inexistente")
+            raise ArquivoError("arquivo inexistente")  # se tiver um error do tipo assertion propaga um erro arquivo Error
 
-        # implementar o problema do leitores e escritores
 
     def escrever_arquivo(self, arquivo, texto):
-        arquivo_objeto = self.__arquivos.get(arquivo)
+        arquivo_objeto = self.__arquivos.get(arquivo)  # pega o arquivo no dicionáro
         try:
-            assert arquivo_objeto is not None
-            arquivo_objeto.escrever_arquivo(texto)
+            assert arquivo_objeto is not None  # se o arquivo não existir solta um AssertionError
+            arquivo_objeto.escrever_arquivo(texto)  # escreve o texto no arquivo
 
         except AssertionError:
-            raise ArquivoError("arquivo inexistente")
+            raise ArquivoError("arquivo inexistente")  # se tiver um error do tipo assertion propaga um erro arquivo Error
 
 
 
--- servidor.py
+++ servidor.py
@@ -14,65 +14,70 @@ sock.listen(50)
 gerenciador_arquivo = Gerenciador()
 
 
-def processa_mensagem(mensagem, coneccao, cliente):
-    mensagem_decodificada = mensagem.decode()
-    mensagem_decodificada = mensagem_decodificada.split()
-    if mensagem_decodificada[0] == "DOWNLOAD" or mensagem_decodificada[0] == "READ":
-        nome_arquivo = "".join(mensagem_decodificada[1:])
-        try:
-            conteudo_arquivo = gerenciador_arquivo.ler_arquivo(nome_arquivo)
-            coneccao.send(str.encode(conteudo_arquivo))
 
-        except ArquivoError as error:
+def processa_mensagem(mensagem, coneccao, cliente): # Metodo que faz o processamento da mensagem
+    mensagem_decodificada = mensagem.decode() #Decodifica a mensagem e coloca dentro da variável, mensagem_decodificada
+    mensagem_decodificada = mensagem_decodificada.split() # Dividi a mensagem decodificada em pequenos pedaços
+
+    if mensagem_decodificada[0] == "READ": #Vai comparar a mensagem que está no índice da posição 0, se é igual a READ
+        nome_arquivo = "".join(mensagem_decodificada[1:]) #Se for igual a READ, vai pegar o valor que está na posição 1 
+        
+        try: #Se existir
+            conteudo_arquivo = gerenciador_arquivo.ler_arquivo(nome_arquivo + ".txt") #Vai ler o conteudo do arquivo.txt e colocalo na variável conteudo do arquivo
+            coneccao.send(str.encode(conteudo_arquivo)) # Vai ser enviado o conteudo do arquivo codificado para o cliente
+
+        except ArquivoError as error: #Caso não exista, vai enviar uma mensagem de erro 404 codificada para o cliente  
+            coneccao.send(str.encode("404")) 
+
+    elif mensagem_decodificada[0] == "DOWNLOAD": # Caso a mensagem que está na posição 0, seja igual DOWNLOAD
+        nome_arquivo = "".join(mensagem_decodificada[1:]) #Percorre o que está no índicie 1 em diante e coloca na variável nome_arquivo
+        try:
+            conteudo_arquivo = gerenciador_arquivo.ler_arquivo(nome_arquivo + ".txt") # Ler o arquivo.txt e coloca seu conteudo na variável conteudo_arquivo
+   
+            coneccao.send(str.encode(f"203 {conteudo_arquivo}"))# Envia uma mensagem codificada, relatando que deu tudo certo, mais o conteudo do arquivo para o cliente
+
+        except ArquivoError as error: #Caso ocorra um erro, vai enviar uma mensagem de erro 404 codificada para o cliente  
             coneccao.send(str.encode("404"))
 
-    elif mensagem_decodificada[0] == "WRITE":
-        nome_arquivo = "".join(mensagem_decodificada[1])
-        dados = " ".join(mensagem_decodificada[2:])
+    elif mensagem_decodificada[0] == "WRITE":  # Caso a mensagem que está no índice 0, seja igual WRITE
+        nome_arquivo = "".join(mensagem_decodificada[1]) #Pega a mensagem que está no índice 1 e coloca na variável nome_arquivo
+        dados = " ".join(mensagem_decodificada[2:]) # Percorre os índices que estão na posição 2 em diante, separando por um espaço e coloca na variável dados
+    
         try:
-            gerenciador_arquivo.escrever_arquivo(nome_arquivo, dados)
-            coneccao.send(str.encode("404"))
+            gerenciador_arquivo.escrever_arquivo(nome_arquivo + ".txt", dados) # Escreve no arquivo.txt
+            coneccao.send(str.encode("205")) #Envia uma mensagem codificada de sucesso para o cliente
+            
 
         except ArquivoError as error:
-            coneccao.send(str.encode(error))
+            coneccao.send(str.encode("404")) #Caso ocorra um erro, envia uma mensagem codificada de erro para o cliente
 
-    elif mensagem_decodificada[0] == "CREATE":
-        nome_arquivo = ''.join(mensagem_decodificada[1])
+    elif mensagem_decodificada[0] == "CREATE": # Caso a mensagem que está no índice 0, seja igual CREATE
+        nome_arquivo = ''.join(mensagem_decodificada[1]) # Pega o que está no íncide 1 e coloca na variável
         try:
-            gerenciador_arquivo.criar_arquivo(nome_arquivo)
-            coneccao.send(str.encode("ARQUIVO_CRIADO " + nome_arquivo))
+            gerenciador_arquivo.criar_arquivo(nome_arquivo + ".txt") #Vai criar um arquivo.txt
+            coneccao.send(str.encode("201")) #Envia uma mensagem de sucesso para o cliente
 
         except ArquivoError as error:
-            coneccao.send(str.encode("444"))
+            coneccao.send(str.encode("444")) #Caso de errado, retorna uma mensagem de erro para o cliente
 
 
-def processa_cliente(con, cliente):
-    while True:
-        msg = con.recv(TAMANHO_MENSAGEM)
+def processa_cliente(con, cliente): #Metodo para processar mensagem do cliente
+    while True: #Enquanto for verdadeiro
+        msg = con.recv(TAMANHO_MENSAGEM) #Ler o tamanho da mensagem do cliente
         if not msg or processa_mensagem(msg, con, cliente): break
 
-    con.close()
+    con.close() #Fecha a conexão e informa qual cliente foi desconecatado
     print("Cliente desconectado:", cliente)
 
 
 while True:
     try:
-        con, cliente = sock.accept()
+        con, cliente = sock.accept() #Inicia uma conexão com o cliente
 
     except: break
-    thread = Thread(target=processa_cliente, args=(con, cliente))
+    thread = Thread(target=processa_cliente, args=(con, cliente)) 
 
-    thread.start()
+    thread.start() #Dá início a uma thread, para cada cliente criado
 
 
 sock.close()
-
-
-
-
-
-
-
-
-
-
